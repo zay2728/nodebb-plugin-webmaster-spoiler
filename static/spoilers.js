febacd921729e21b706341bf9674e598f4cc38a4
KomunitasWebmaster <mzainudin27@gmail.com>
2015-01-13 12:47:06 +0700

    update baru

--- static/spoilers.js
+++ static/spoilers.js
@@ -2,10 +2,10 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, ">## \n> text kamu disini");
+				controls.insertIntoTextarea(textarea, "> spoiler \n>## \n> text kamu disini");
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 12);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '>## \n>','\n>');
+				controls.wrapSelectionInTextareaWith(textarea, '> spoiler\n >## \n>','\n>');
 				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
 			}
 		});
