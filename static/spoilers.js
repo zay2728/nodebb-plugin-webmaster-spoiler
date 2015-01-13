4b0b176d77a520a14252eb8bf3f30d77a5e95e7c
KomunitasWebmaster <mzainudin27@gmail.com>
2015-01-13 14:11:45 +0700

    fixed

--- static/spoilers.js
+++ static/spoilers.js
@@ -3,10 +3,10 @@ $('document').ready(function() {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
 				controls.insertIntoTextarea(textarea, "> spoiler \n>## \n> text kamu disini");
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionStart + 12);
+				controls.updateTextareaSelection(textarea, selectionStart + 15, selectionStart + 16);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '> spoiler\n >## \n>','\n>');
-				controls.updateTextareaSelection(textarea, selectionStart + 3, selectionEnd + 3);
+				controls.wrapSelectionInTextareaWith(textarea, '> spoiler\n>## \n>','\n> ');
+				controls.updateTextareaSelection(textarea, selectionStart + 15, selectionEnd + 15);
 			}
 		});
 	});
