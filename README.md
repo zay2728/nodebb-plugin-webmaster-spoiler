7bdf0bee8116dea30b4744010fac76d57a41ac80
KomunitasWebmaster <mzainudin27@gmail.com>
2015-01-16 08:02:31 +0700

    update
    now command just
    >spoiler
    >#
    > your text here

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 membuat kamu bisa menggunakan spoiler :D di nodebb v0.6
 
 ```
->##
+>#
 > spoiler text here
 ```
 
--- static/spoilers.js
+++ static/spoilers.js
@@ -2,11 +2,11 @@ $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
 		composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, "> spoiler \n>## \n> text kamu disini");
-				controls.updateTextareaSelection(textarea, selectionStart + 17, selectionStart + 34);
+				controls.insertIntoTextarea(textarea, "> spoiler \n># \n> text kamu disini");
+				controls.updateTextareaSelection(textarea, selectionStart + 16, selectionStart + 33);
 			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '> spoiler \n>## \n>','\n>');
-				controls.updateTextareaSelection(textarea, selectionStart + 17, selectionEnd + 17);
+				controls.wrapSelectionInTextareaWith(textarea, '> spoiler \n># \n>','\n>');
+				controls.updateTextareaSelection(textarea, selectionStart + 16, selectionEnd + 16);
 			}
 		});
 	});
